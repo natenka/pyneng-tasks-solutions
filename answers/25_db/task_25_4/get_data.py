c25f69a1f0b6ef935ddd0ae27c6e74771ae88eff
natenka <nataliya.samoylenko@gmail.com>
2022-10-09 16:40:03 +0000

    25 done

--- answers/25_db/task_25_4/get_data.py
+++ answers/25_db/task_25_4/get_data.py
@@ -7,22 +7,20 @@ from tabulate import tabulate
 def print_data_in_rows(data, active=True):
     data = list(data)
     if data:
-        print(
-            "\n{active} записи:\n".format(active="Активные" if active else "Неактивные")
-        )
+        print("\n{active} entries:\n".format(active="Active" if active else "Inactive"))
         print(tabulate(data))
 
 
 def get_data_by_key_value(db_name, key, value):
     keys = "mac ip vlan interface switch".split()
     if key not in keys:
-        print("Данный параметр не поддерживается.")
-        print("Допустимые значения параметров: {}".format(", ".join(keys)))
+        print("This parameter is not supported.")
+        print("Valid parameter values: {}".format(", ".join(keys)))
         return
     conn = sqlite3.connect(db_filename)
     query = "select * from dhcp where {} = ? and active = ?".format(key)
 
-    print("\nИнформация об устройствах с такими параметрами:", key, value)
+    print("\nInformation about devices with the following parameters:", key, value)
     for active in (1, 0):
         result = conn.execute(query, (value, active))
         print_data_in_rows(result, active)
@@ -30,7 +28,7 @@ def get_data_by_key_value(db_name, key, value):
 
 
 def get_all_data(db_name):
-    print("В таблице dhcp такие записи:")
+    print("The dhcp table has the following entries:")
     query = "select * from dhcp where active = ?"
     conn = sqlite3.connect(db_name)
     for active in (1, 0):
@@ -46,7 +44,7 @@ def parse_args(db_name, args):
         key, value = args
         get_data_by_key_value(db_filename, key, value)
     else:
-        print("Пожалуйста, введите два или ноль аргументов")
+        print("Please enter two or zero arguments")
 
 
 if __name__ == "__main__":
