dd07afd5632f6c3d9964765fa7fb37632f65d168
natenka <nataliya.samoylenko@gmail.com>
2022-10-23 10:55:48 +0000

    04 done

--- answers/04_data_structures/task_4_2.py
+++ answers/04_data_structures/task_4_2.py
@@ -5,6 +5,6 @@ nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
 nat_gig = nat.replace("Fast", "Gigabit")
 print(nat_gig.lower())
 
-# вариант
+# version 2
 # nat_gig = nat.lower().replace("fast", "gigabit")
 # print(nat_gig)
--- answers/04_data_structures/task_4_6.py
+++ answers/04_data_structures/task_4_6.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
 template = """
 Prefix                {}
