dbef95558be0a96d7437b0765c4a2938780233aa
natenka <nataliya.samoylenko@gmail.com>
2022-10-23 14:23:51 +0000

    5.1 5.2 fixed

--- answers/05_basic_scripts/task_5_1.py
+++ answers/05_basic_scripts/task_5_1.py
@@ -4,6 +4,6 @@ words = [
     'Guido', 'van', 'Rossum', 'began', 'working', 'on',
     'Python', 'in', 'the', 'late', '1980s',
 ]
-index = int(input("Введите индекс: "))
+index = int(input("Enter index: "))
 word = words[index]
 print(word.lower())
--- answers/05_basic_scripts/task_5_2.py
+++ answers/05_basic_scripts/task_5_2.py
@@ -4,5 +4,5 @@ words = [
     'Guido', 'van', 'Rossum', 'began', 'working', 'on',
     'Python', 'in', 'the', 'late', '1980s',
 ]
-sep = input("Введите разделитель: ")
+sep = input("Enter separator: ")
 print(sep.join(words))
