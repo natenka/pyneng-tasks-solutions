be302ff50b9fcf255e39f9d278dac9a1680ce470
natenka <nataliya.samoylenko@gmail.com>
2023-03-08 10:30:16 +0000

    UPD answers

--- answers/19_concurrent_connections/task_19_4.py
+++ answers/19_concurrent_connections/task_19_4.py
@@ -25,13 +25,17 @@ def send_cfg_commands(device, commands):
 def send_commands_to_devices(devices, filename, *, show=None, config=None, limit=3):
     if show and config:
         raise ValueError("Only one of the show/config arguments can be passed")
-    command = show if show else config
-    function = send_show_command if show else send_cfg_commands
+    elif show is None and config is None:
+        raise ValueError("You need to pass one of the show/config arguments")
 
     with ThreadPoolExecutor(max_workers=limit) as executor:
-        futures = [executor.submit(function, device, command) for device in devices]
+        if show:
+            futures = [executor.submit(send_show_command, device, show) for device in devices]
+        elif config:
+            futures = [executor.submit(send_cfg_commands, device, config) for device in devices]
+
         with open(filename, "w") as f:
-            for future in as_completed(futures):
+            for future in futures:
                 f.write(future.result())
 
 
